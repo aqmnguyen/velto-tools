2351f6d3b6a313eb5e0658552c6a6230ff7d8692
Michael Nguyen <aqmnguyen@gmail.com>
2025-06-28 23:54:47 +0100

    stub out any post hog events in tests

--- apps/web/cypress/support/commands.ts
+++ apps/web/cypress/support/commands.ts
@@ -1,1 +1,20 @@
-export {};
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      stubPostHog(): void;
+    }
+  }
+}
+
+// This command blocks all requests to any PostHog domain
+Cypress.Commands.add('stubPostHog', () => {
+  cy.intercept('POST', '**.posthog.com/**', {
+    statusCode: 200,
+    body: {},
+  });
+
+  cy.intercept('GET', '**.posthog.com/**', {
+    statusCode: 200,
+    body: {},
+  });
+});
